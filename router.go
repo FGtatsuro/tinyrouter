86554d477a2b1b096ff74621eb16c8dec4f50f8c
FGtatsuro <204491+FGtatsuro@users.noreply.github.com>
2020-06-05 00:54:40 +0900

    Implement exact match for path

--- router.go
+++ router.go
@@ -2,17 +2,25 @@ package tinyrouter
 
 import (
 	"net/http"
+	"path"
 )
 
 type Router struct {
+	handlerMap map[string]*http.Handler
 }
 
 func New() *Router {
-	return &Router{}
+	return &Router{make(map[string]*http.Handler)}
 }
 
-func (r *Router) Handle(pattern string, handler http.Handler) {
+func (router *Router) Handle(pattern string, handler http.Handler) {
+	router.handlerMap[path.Clean(pattern)] = &handler
 }
 
-func (r *Router) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+func (router *Router) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	router.Handle(pattern, http.HandlerFunc(handler))
+}
+
+func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	(*router.handlerMap[path.Clean(r.URL.Path)]).ServeHTTP(w, r)
 }
