f5bb86f2a35c13aeceba7131bd124ee628d5cec2
FGtatsuro <204491+FGtatsuro@users.noreply.github.com>
2020-06-06 00:17:34 +0900

    Add a test for multiple routes registration.
    Point: Same path overwrites previous registered route.

--- router_test.go
+++ router_test.go
@@ -86,3 +86,44 @@ func TestHandleFunc(t *testing.T) {
 	}
 
 }
+
+func TestMultipleRoutes(t *testing.T) {
+	writes := []string{
+		"root",
+		"root_multiple",
+		"next",
+		"next/follow",
+	}
+	wants := []string{
+		// Same path overwrites previous registered route.
+		"root_multiple",
+		"root_multiple",
+		"next",
+		"next/follow",
+	}
+	router := tinyrouter.New()
+	for i, path := range paths {
+		// FYI: https://github.com/golang/go/wiki/CommonMistakes
+		write := writes[i]
+		router.HandleFunc(
+			path,
+			func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte(write))
+			})
+	}
+	for i, path := range paths {
+		t.Run(path, func(t *testing.T) {
+			s := httptest.NewServer(router)
+			defer s.Close()
+			resp, _ := s.Client().Get(s.URL + path)
+
+			defer resp.Body.Close()
+			body, _ := ioutil.ReadAll(resp.Body)
+
+			got := string(body)
+			if wants[i] != got {
+				t.Errorf("Handler binding to '%v' must be called: want: %v/got %v", path, wants[i], got)
+			}
+		})
+	}
+}
