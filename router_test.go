949dd53e205d909fafd780aa885d8e95b28ead2e
FGtatsuro <204491+FGtatsuro@users.noreply.github.com>
2020-06-06 01:13:53 +0900

    Define route/testcase type for test readability.

--- router_test.go
+++ router_test.go
@@ -17,118 +17,129 @@ func TestNew(t *testing.T) {
 	}
 }
 
-var paths = []string{
-	"/",
-	"//",
-	"/next",
-	"/next/follow",
+type route struct {
+	path  string
+	write string
+}
+
+type testcase struct {
+	path string
+	want string
 }
 
 func TestHandle(t *testing.T) {
-	writes := []string{
-		"root",
-		"root_multiple",
-		"next",
-		"next/follow",
+	routes := []route{
+		{"/", "root"},
+		{"//", "root_multiple"},
+		{"/next", "next"},
+		{"/next/follow", "next/follow"},
 	}
-	wants := writes
-	for i, path := range paths {
+	var testcases []testcase
+	for _, route := range routes {
+		testcases = append(testcases, testcase{route.path, route.write})
+	}
+
+	for i, route := range routes {
 		// FYI: https://github.com/golang/go/wiki/CommonMistakes
-		write := writes[i]
-		t.Run(path, func(t *testing.T) {
+		write := route.write
+		t.Run(route.path, func(t *testing.T) {
 			router := tinyrouter.New()
 			router.Handle(
-				path,
+				route.path,
 				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 					w.Write([]byte(write))
 				}))
 
 			s := httptest.NewServer(router)
 			defer s.Close()
-			resp, _ := s.Client().Get(s.URL + path)
+			resp, _ := s.Client().Get(s.URL + testcases[i].path)
 
 			defer resp.Body.Close()
 			body, _ := ioutil.ReadAll(resp.Body)
 
 			got := string(body)
-			if wants[i] != got {
-				t.Errorf("Handler binding to '%v' must be called: want: %v/got %v", path, wants[i], got)
+			if testcases[i].want != got {
+				t.Errorf("Handler binding to '%v' must be called: want: %v/got %v", testcases[i].path, testcases[i].want, got)
 			}
 		})
 	}
 }
 
 func TestHandleFunc(t *testing.T) {
-	writes := []string{
-		"root",
-		"root_multiple",
-		"next",
-		"next/follow",
+	routes := []route{
+		{"/", "root"},
+		{"//", "root_multiple"},
+		{"/next", "next"},
+		{"/next/follow", "next/follow"},
 	}
-	wants := writes
-	for i, path := range paths {
+	var testcases []testcase
+	for _, route := range routes {
+		testcases = append(testcases, testcase{route.path, route.write})
+	}
+
+	for i, route := range routes {
 		// FYI: https://github.com/golang/go/wiki/CommonMistakes
-		write := writes[i]
-		t.Run(path, func(t *testing.T) {
+		write := route.write
+		t.Run(route.path, func(t *testing.T) {
 			router := tinyrouter.New()
 			router.HandleFunc(
-				path,
+				route.path,
 				func(w http.ResponseWriter, r *http.Request) {
 					w.Write([]byte(write))
 				})
 
 			s := httptest.NewServer(router)
 			defer s.Close()
-			resp, _ := s.Client().Get(s.URL + path)
+			resp, _ := s.Client().Get(s.URL + testcases[i].path)
 
 			defer resp.Body.Close()
 			body, _ := ioutil.ReadAll(resp.Body)
 
 			got := string(body)
-			if wants[i] != got {
-				t.Errorf("Handler binding to '%v' must be called: want: %v/got %v", path, wants[i], got)
+			if testcases[i].want != got {
+				t.Errorf("Handler binding to '%v' must be called: want: %v/got %v", testcases[i].path, testcases[i].want, got)
 			}
 		})
 	}
-
 }
 
 func TestMultipleRoutes(t *testing.T) {
-	writes := []string{
-		"root",
-		"root_multiple",
-		"next",
-		"next/follow",
+	routes := []route{
+		{"/", "root"},
+		{"//", "root_multiple"},
+		{"/next", "next"},
+		{"/next/follow", "next/follow"},
 	}
-	wants := []string{
+	testcases := []testcase{
 		// Same path overwrites previous registered route.
-		"root_multiple",
-		"root_multiple",
-		"next",
-		"next/follow",
+		{"/", "root_multiple"},
+		{"//", "root_multiple"},
+		{"/next", "next"},
+		{"/next/follow", "next/follow"},
 	}
+
 	router := tinyrouter.New()
-	for i, path := range paths {
+	for _, route := range routes {
 		// FYI: https://github.com/golang/go/wiki/CommonMistakes
-		write := writes[i]
+		write := route.write
 		router.HandleFunc(
-			path,
+			route.path,
 			func(w http.ResponseWriter, r *http.Request) {
 				w.Write([]byte(write))
 			})
 	}
-	for i, path := range paths {
-		t.Run(path, func(t *testing.T) {
+	for _, tc := range testcases {
+		t.Run(tc.path, func(t *testing.T) {
 			s := httptest.NewServer(router)
 			defer s.Close()
-			resp, _ := s.Client().Get(s.URL + path)
+			resp, _ := s.Client().Get(s.URL + tc.path)
 
 			defer resp.Body.Close()
 			body, _ := ioutil.ReadAll(resp.Body)
 
 			got := string(body)
-			if wants[i] != got {
-				t.Errorf("Handler binding to '%v' must be called: want: %v/got %v", path, wants[i], got)
+			if tc.want != got {
+				t.Errorf("Handler binding to '%v' must be called: want: %v/got %v", tc.path, tc.want, got)
 			}
 		})
 	}
