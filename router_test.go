a616e84179cc2a671fb808608665d59de32f739b
FGtatsuro <204491+FGtatsuro@users.noreply.github.com>
2020-06-05 23:36:35 +0900

    Fix variable names for multiple routes test.

--- router_test.go
+++ router_test.go
@@ -17,62 +17,72 @@ func TestNew(t *testing.T) {
 	}
 }
 
-var testcases = []struct {
-	path string
-	want string
-}{
-	{"/", "root"},
-	{"//", "root"},
-	{"/next", "next"},
-	{"/next/follow", "next/follow"},
+var paths = []string{
+	"/",
+	"//",
+	"/next",
+	"/next/follow",
 }
 
 func TestHandle(t *testing.T) {
-	for _, tc := range testcases {
-		t.Run(tc.path, func(t *testing.T) {
+	wants := []string{
+		"root",
+		"root_multiple",
+		"next",
+		"next/follow",
+	}
+	for i, path := range paths {
+		t.Run(path, func(t *testing.T) {
 			router := tinyrouter.New()
 			router.Handle(
-				tc.path,
+				path,
 				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					w.Write([]byte(tc.want))
+					w.Write([]byte(wants[i]))
 				}))
 
 			s := httptest.NewServer(router)
 			defer s.Close()
-			resp, _ := s.Client().Get(s.URL + tc.path)
+			resp, _ := s.Client().Get(s.URL + path)
 
 			defer resp.Body.Close()
 			body, _ := ioutil.ReadAll(resp.Body)
 
 			got := string(body)
-			if tc.want != got {
-				t.Errorf("Handler binding to '%v' must be called: want: %v/got %v", tc.path, tc.want, got)
+			if wants[i] != got {
+				t.Errorf("Handler binding to '%v' must be called: want: %v/got %v", path, wants[i], got)
 			}
 		})
 	}
 }
 
 func TestHandleFunc(t *testing.T) {
-	for _, tc := range testcases {
-		t.Run(tc.path, func(t *testing.T) {
+	wants := []string{
+		"root",
+		"root_multiple",
+		"next",
+		"next/follow",
+	}
+	for i, path := range paths {
+		t.Run(path, func(t *testing.T) {
 			router := tinyrouter.New()
 			router.HandleFunc(
-				tc.path,
+				path,
 				func(w http.ResponseWriter, r *http.Request) {
-					w.Write([]byte(tc.want))
+					w.Write([]byte(wants[i]))
 				})
 
 			s := httptest.NewServer(router)
 			defer s.Close()
-			resp, _ := s.Client().Get(s.URL + tc.path)
+			resp, _ := s.Client().Get(s.URL + path)
 
 			defer resp.Body.Close()
 			body, _ := ioutil.ReadAll(resp.Body)
 
 			got := string(body)
-			if tc.want != got {
-				t.Errorf("Handler binding to '%v' must be called: want: %v/got %v", tc.path, tc.want, got)
+			if wants[i] != got {
+				t.Errorf("Handler binding to '%v' must be called: want: %v/got %v", path, wants[i], got)
 			}
 		})
 	}
+
 }
